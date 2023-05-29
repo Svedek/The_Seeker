509c803c29e73b913c05f76571144999b3540d6c
Svedek <naroon10@gmail.com>
2023-05-28 20:52:10 -0500

    toCurrent

--- The_Seeker/src/Player/player.gd
+++ The_Seeker/src/Player/player.gd
@@ -18,7 +18,7 @@ func _handle_input():
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var axis = Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
