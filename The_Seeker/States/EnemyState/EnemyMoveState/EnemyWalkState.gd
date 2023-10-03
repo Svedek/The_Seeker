d9a8a05b4e22f80a0ce0b46c084733290cd8cd1f
Svedek <28320440+Svedek@users.noreply.github.com>
2023-10-03 18:31:37 -0500

    wubbla state changes deon

--- The_Seeker/States/EnemyState/EnemyMoveState/EnemyWalkState.gd
+++ The_Seeker/States/EnemyState/EnemyMoveState/EnemyWalkState.gd
@@ -1,11 +1,36 @@
-extends Node
+extends EnemyConstantMoveState
 
+# Goes to Idle, Dash, Attack, Aim
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+@export_category("State Transitions")
+@export var idle_node: NodePath
+@export var dash_node: NodePath
+@export var attack_node: NodePath
+@export var aim_node: NodePath
 
+@onready var idle_state: BaseState = get_node(idle_node)
+@onready var dash_state: BaseState = get_node(dash_node)
+@onready var attack_state: BaseState = get_node(attack_node)
+@onready var aim_state: BaseState = get_node(aim_node)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func input(event):
+	if event.is_action_pressed("dash") && dash_state.available:
+		dir = get_axis()
+		return dash_state
+	elif event.is_action_pressed("attack") && attack_state.available:
+		attack_state.input(event) # ?
+		return attack_state # Some way to sense the direction of the attack (in attack or before call)
+	elif event.is_action_pressed("aim") && aim_state.available:
+		return aim_state
+	
+func physics_process(delta: float) -> BaseState:
+	var axis = get_axis()
+	if (axis == Vector2.ZERO): # IDLE
+		return idle_state
+		
+	dir = axis.normalized()
+	character.set_blend_position(animation_name, dir)
+	
+	character.velocity = axis * character.stats.move_speed
+	character.move_and_slide()
+	return null
