f5e9f3fa18f0099a72d26c4083602105a359b21b
Svedek <28320440+Svedek@users.noreply.github.com>
2024-08-04 17:02:41 -0500

    Basic menu navigation

--- The_Seeker/Actors/Menus+GUI/main_menu.gd
+++ The_Seeker/Actors/Menus+GUI/main_menu.gd
@@ -1,6 +1,13 @@
 extends Control
 
 
+@onready var menu_hallway = $SubViewport/MenuHallway
+@onready var title_screen = $TitleScreen
+@onready var options_screen = $OptionsScreen
+
+const ARENA = preload("res://Actors/World/arena.tscn")
+
+
 func _ready():
 	recenter()
 
@@ -11,3 +18,37 @@ func _process(delta):
 
 func recenter():
 	position = -size/2
+
+
+func _on_start_button_pressed():
+	menu_hallway.animate_play()
+
+
+func _on_options_button_pressed():
+	menu_hallway.animate_options_enter()
+
+
+func _on_options_return_button_pressed():
+	menu_hallway.animate_options_exit()
+
+
+func _on_exit_button_pressed():
+	menu_hallway.animate_exit()
+
+
+func _on_menu_hallway_start_reached():
+	get_tree().change_scene_to_packed(ARENA)
+
+
+func _on_menu_hallway_options_reached():
+	title_screen.visible = false
+	options_screen = true
+
+
+func _on_menu_hallway_options_left():
+	title_screen.visible = true
+	options_screen = false
+
+
+func _on_menu_hallway_exit_reached():
+	get_tree().quit()
