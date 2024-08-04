adbf91805e2e6439886a437f6ae8b997d32e41af
Svedek <28320440+Svedek@users.noreply.github.com>
2024-08-04 16:03:48 -0500

    Hallway animations are now animatable by script

--- The_Seeker/Menus+GUI/menu_hallway.gd
+++ The_Seeker/Menus+GUI/menu_hallway.gd
@@ -1,9 +1,23 @@
 extends Node2D
 
 
-func _ready():
-	pass
+signal start_reached
+signal options_reached
+signal options_left
+signal exit_reached
 
+@onready var _animation_tree = $AnimationTree
+@onready var _animation_playback : AnimationNodeStateMachinePlayback = _animation_tree.get("parameters/playback")
 
-func _process(delta):
-	pass
+func animate_play():
+	_animation_playback.travel("play")
+
+func animate_options_enter():
+	_animation_playback.travel("options_enter")
+
+func animate_options_exit():
+	_animation_playback.travel("options_exit")
+
+func animate_exit():
+	_animation_playback.travel("exit")
+	
